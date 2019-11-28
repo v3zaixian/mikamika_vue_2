22c2048c1e187913f5d25c9bf6926f6fb90b07ba
xiaodi <676585960@qq.com>
2019-11-28 19:44:29 +0800

    ggg

--- src/components/Lunbo/Lunbo.vue
+++ src/components/Lunbo/Lunbo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Container">
-    <el-carousel :class="[swiperContainer]" :interval="1000" arrow="always" indicator-position="none">
+    <el-carousel height="258px" :class="[swiperContainer]" :interval="1000" arrow="always" indicator-position="none">
       <el-carousel-item class="el-carousel__item" v-for="(item,index) in dataimg" :key="index">
         <img class="swiperImg" :src="item.item_src" alt="">
       </el-carousel-item>
